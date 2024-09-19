5021c951721accc9797107bd7e23b168b940c819
Daniel Teixeira <danielbteixeira91@gmail.com>
2024-09-19 16:13:57 -0300

    feat: adiciona projeto contaBanco

---
+++ desafios/sintaxe/ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo = 674.95;
+
+        System.out.println("Por favor, digite o número da Conta:");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numero + " e seu saldo de R$ " + saldo + " já está disponível para saque.");
+
+        scanner.close();
+
+
+    }
+}
